7edc1d9872979e1adf45321db6e6bbc4a69f28c9
EgorAndrik <egorandreas28@gmail.com>
2023-10-07 01:15:27 +0700

    Adding a page for uploading data and a requirements.txt file

--- server.py
+++ server.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from json import load, dump
 
 
 application = Flask(__name__, template_folder='templates')
 
 
+# @application.route('/test/<filename>')
+# def testDownload(filename):
+    # return send_from_directory('Users', filename) # csv xlsx xls
+
+
 @application.route('/')
-def homePage():
+def homePage() -> str:
     return render_template('index.html')
 
 
 @application.route('/LogInPage')
-def LogInPage():
+def LogInPage() -> str:
     return render_template('loginIndex.html')
 
 
 @application.route('/LogInPage/LogIn', methods=['POST'])
-def LogIn():
+def LogIn() -> str:
     formData = request.form
     userName, password = [formData[i] for i in formData]
     with open('Users/UsersData.json', 'r', encoding='utf-8') as users:
@@ -25,12 +30,12 @@ def LogIn():
 
 
 @application.route('/RegistrationPage')
-def registrPage():
+def registrPage() -> str:
     return render_template('registrationIndex.html')
 
 
 @application.route('/RegistrationPage/Registration', methods=['POST'])
-def registr():
+def registr() -> str:
     formData = request.form
     userName, password, dateBorn = [formData[i] for i in formData]
     with open('Users/UsersData.json', 'r', encoding='utf-8') as users:
@@ -51,7 +56,7 @@ def registr():
 
 
 @application.route('/userPage/<UserName>')
-def userPage(UserName: str):
+def userPage(UserName: str) -> str:
     with open('Users/UsersData.json', 'r', encoding='utf-8') as users:
         usersData = load(users)
     userData = usersData[UserName][-1]
@@ -70,12 +75,13 @@ def userPage(UserName: str):
         chart_income_data=chart_income_data,
         chart_ratio_data=chart_ratio_data,
         expensesLink=f'/userPage/{UserName}/Расходы',
-        incomeLink=f'/userPage/{UserName}/Доходы'
+        incomeLink=f'/userPage/{UserName}/Доходы',
+        getDataUser=f'/getDataUser/{UserName}'
     )
 
 
 @application.route('/userPage/<UserName>/<formForGraph>')
-def formPage(UserName: str, formForGraph: str):
+def formPage(UserName: str, formForGraph: str) -> str:
     return render_template(
         'expenses_income.html',
         operationName=formForGraph,
@@ -85,7 +91,7 @@ def formPage(UserName: str, formForGraph: str):
 
 
 @application.route('/userData/<UserName>/<formForGraph>', methods=['POST'])
-def addUserData(UserName: str, formForGraph: str):
+def addUserData(UserName: str, formForGraph: str) -> str:
     with open('Users/UsersData.json', 'r', encoding='utf-8') as users:
         usersData = load(users)
     userForm = request.form
@@ -105,6 +111,14 @@ def addUserData(UserName: str, formForGraph: str):
             indent='\t'
         )
     return formPage(UserName, formForGraph)
+
+
+@application.route('/getDataUser/<UserName>')
+def getDataUser(UserName: str):
+    return render_template(
+        'getDataUsers.html',
+        homeUserPage=f'/userPage/{UserName}'
+    )
 
 
 if __name__ == '__main__':

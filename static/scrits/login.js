4585c8d35d3fce8df5889b83282e0d1af86c52cd
Egor Andreasyan <124351915+EgorAndrik@users.noreply.github.com>
2024-07-11 17:27:36 +0700

    Update login.js

--- static/scrits/login.js
+++ static/scrits/login.js
@@ -17,23 +17,3 @@ document.getElementById('login-form').addEventListener('submit', async function(
         document.getElementById('error-message').innerText = result.message;
     }
 });
-
-document.getElementById('registr-form').addEventListener('submit', async function(event) {
-    event.preventDefault(); // Prevent default form submission
-
-    const form = event.target;
-    const formData = new FormData(form);
-
-    const response = await fetch('/RegistrationPage/Registration', {
-        method: 'POST',
-        body: formData
-    });
-
-    const result = await response.json();
-
-    if (result.success) {
-        window.location.href = `/userPage/${result.userName}`;
-    } else {
-        document.getElementById('error-message').innerText = result.message;
-    }
-});
